024cddd967db80d7bb88a7bffc846b0c7a5c291a
Lucas <lucasgunner3@gmail.com>
2021-08-04 00:31:42 -0300

    Criando as interfaces de serviço

---
+++ GameCatalog.API/Domain/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameCatalog.API.Domain.Entities;
+
+namespace GameCatalog.API.Domain.Interfaces.Services
+{
+    public interface IBaseService<T> where T : Base
+    {
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Delete(Guid id);
+        Task<T> GetById(Guid id);
+        Task<IEnumerable<T>> Get(int page, int amount);
+        Task<bool> Exists(Guid id);
+    }
+}
---
+++ GameCatalog.API/Domain/Interfaces/Services/IGameService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using GameCatalog.API.Domain.Entities;
+
+namespace GameCatalog.API.Domain.Interfaces.Services
+{
+    public interface IGameService : IBaseService<Game>
+    {
+        Task UpdatePrice(Guid id, double price);
+        Task UpdateTitle(Guid id, string title);
+    }
+}
