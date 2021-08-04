afabbd040f31da0e8ac3b36b9e930519035fcc83
Lucas <lucasgunner3@gmail.com>
2021-08-03 23:15:26 -0300

    Adicionar arquivos de projeto.

---
+++ GameCatalog.API/Controllers/V1/GamesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameCatalog.API.Controllers.V1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        public GamesController()
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return null;
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Put(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPatch("{id:guid}/price/{price:double}")]
+        public async Task<IActionResult> PatchPrice(Guid id, double price)
+        {
+            return null;
+        }
+
+        [HttpPatch("{id:guid}/title/{title}")]
+        public async Task<IActionResult> PatchPrice(Guid id, string title)
+        {
+            return null;
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return null;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return null;
+        }
+
+
+    }
+}
---
+++ GameCatalog.API/Models/Games/InputModels/UpdateGameInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameCatalog.API.Models.Games.InputModels
+{
+    public class UpdateGameInputModel                             
+    {
+        [Required(ErrorMessage = "The field {0} is required.")]
+        public Guid Id { get; set; }
+        [StringLength(40, ErrorMessage = "The name field {0} must be at least {2} and a maximum of {1} characters.", MinimumLength = 3)]
+        [Required(ErrorMessage = "The field {0} is required.")]
+        public string Name { get; set; }
+        [StringLength(100, ErrorMessage = "The name field {0} must be at least {2} and a maximum of {1} characters.", MinimumLength = 2)]
+        [Required(ErrorMessage = "The field {0} is required.")]
+        public string Developer { get; set; }
+        [Range(minimum: 0.00, maximum: 100000.00, ConvertValueInInvariantCulture = true, ErrorMessage = "The field {0} must be between {2} and {1}")]
+        [Required(ErrorMessage = "The field {0} is required.")]
+        public double Price { get; set; }
+    }
+}
