1d767b76b82f607ff77edf39bb1ad55b88b2d70f
Lucas <lucasgunner3@gmail.com>
2021-08-04 00:32:39 -0300

    Configurando mapeamento de models para entidades

---
+++ GameCatalog.API/ProfilesConfiguration/MapperConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GameCatalog.API.Domain.Entities;
+using GameCatalog.API.Models.Games.InputModels;
+using GameCatalog.API.Models.Games.ViewModels;
+
+namespace GameCatalog.API.ProfilesConfiguration
+{
+    public class MapperConfiguration : Profile
+    {
+        public MapperConfiguration()
+        {
+            CreateMap<AddGameInputModel, Game>();
+            CreateMap<UpdateGameInputModel, Game>();
+            CreateMap<GameViewModel,Game>().ReverseMap();
+        }   
+    }
+}
