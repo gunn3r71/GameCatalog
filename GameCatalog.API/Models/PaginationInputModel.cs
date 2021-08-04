c2f3f04ebcd406b03390d9267d670f2c8e9da839
Lucas <lucasgunner3@gmail.com>
2021-08-04 00:32:08 -0300

    Criando input model para paginação

---
+++ GameCatalog.API/Models/PaginationInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameCatalog.API.Models
+{
+    public class PaginationInputModel
+    {
+        [Range(1, int.MaxValue)]
+        public int Page { get; set; } = 1;
+        [Range(minimum: 1, maximum: 50, ConvertValueInInvariantCulture = true, ErrorMessage = "The field {0} must be between {2}")]
+        public int Amount { get; set; } = 1;
+    }
+}
